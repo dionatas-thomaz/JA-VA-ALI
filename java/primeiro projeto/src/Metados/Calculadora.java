c0f5d6ebac77c1e051ceda20ee3a380d6b0ca3df
dionatas-thomaz <dionatassanto0@gmail.com>
2024-07-23 10:56:12 -0300

    adc

--- java/primeiro projeto/src/Metados/Calculadora.java
+++ java/primeiro projeto/src/Metados/Calculadora.java
@@ -4,4 +4,7 @@ public class Calculadora {
     public void somaDoisNumeros(){
         System.out.println(10+10);
     }
+    public void multiplicaDoisNumeros(int a, int b){
+        System.out.println(a*b);
+    }
 }
---
+++ java/primeiro projeto/src/Metados/CalculoTest/CalculoTest02.java
@@ -0,0 +1,10 @@
+package Metados.CalculoTest;
+
+import Metados.Calculadora;
+
+public class CalculoTest02 {
+          public static void main(String[] args) {
+            Calculadora calc = new Calculadora();
+            calc.multiplicaDoisNumeros(3,5);
+          }
+}
