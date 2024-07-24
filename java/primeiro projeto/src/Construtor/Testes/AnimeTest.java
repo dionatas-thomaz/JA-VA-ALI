243ad7c8adea16a330098830bdbdd474e8ad2411
dionatas-thomaz <dionatassanto0@gmail.com>
2024-07-24 10:53:42 -0300

    adc

---
+++ java/primeiro projeto/src/Construtor/Testes/AnimeTest.java
@@ -0,0 +1,11 @@
+package Construtor.Testes;
+
+import Construtor.Dominio.Animes;
+
+public class AnimeTest {
+    public static void main(String[] args) {
+       Animes anime = new Animes("boku no piko","gay",12,"ghibli");
+
+        anime.imprime();
+    }
+}
