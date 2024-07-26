458bcd9674194a3070619038f50d863c364d7453
dionatas-thomaz <dionatassanto0@gmail.com>
2024-07-26 12:40:22 -0300

    adc

--- java/primeiro projeto/src/Modificadorestatisco/StaticoTest/CaarroTest01.java
+++ java/primeiro projeto/src/Modificadorestatisco/StaticoTest/CaarroTest01.java
@@ -5,7 +5,7 @@ import Modificadorestatisco.Dominio.Carro;
 public class CaarroTest01 {
         public static void main(String[] args) {
         Carro carro1 = new Carro("BMW",280);
-        Carro carro2 = new Carro("Mercedes",275);
+        Carro carro2 = new Carro("Mercedess",275);
         Carro carro3 = new Carro("audi",290);
             Carro.velocidadeLimine=180;
         carro1.imprime();
